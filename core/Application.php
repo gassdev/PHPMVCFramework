6690e7ed5c648d40274a86cbe6e0c043bd292f7a
gassdev <gassdev@gmail.com>
2021-11-07 12:30:25 +0000

    First commit

---
+++ core/Application.php
@@ -0,0 +1,20 @@
+<?php
+namespace app\core;
+
+/**
+ * Application
+ */
+class Application
+{
+    public Router $router;
+
+    public function __construct()
+    {
+        $this->router = new Router();
+    }
+
+    public function run()
+    {
+        $this->router->resolve();
+    }
+}
