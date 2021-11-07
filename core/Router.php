cbc6f6f14542b83fcbdbd06d077fc50e001c775b
gassdev <gassdev@gmail.com>
2021-11-07 12:51:47 +0000

    handle request with Request class

--- core/Router.php
+++ core/Router.php
@@ -6,7 +6,13 @@ namespace app\core;
  */
 class Router
 {
+    public Request $request;
     protected array $routes = [];
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
 
     public function get($path, $callback)
     {
@@ -15,8 +21,15 @@ class Router
 
     public function resolve()
     {
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
+        $callback = $this->routes[$method][$path] ?? false;
+        if ($callback === false) {
+            echo 'Not Found';
+            exit();
+        }
         echo '<pre>';
-        var_dump($_SERVER);
+        var_dump($callback);
         echo '</pre>';
     }
 }
